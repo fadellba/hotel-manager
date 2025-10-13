c58c04119b40ae9d361f66e46c98e1a4b0c4cdf0
fadel <fafadel230@gmail.com>
2025-10-13 03:17:14 -1200

    devoir S3 l2gl

---
+++ database/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "meeting_rooms_db";
+$port = "3306";
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db";
+
+try {
+    $db = new PDO($dsn, $user, $password);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
